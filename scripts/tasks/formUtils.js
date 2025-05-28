c0e7b5b6525421d860e140cfb649d3e3b2ed3c53
tawandakasunzuma <tskasunzuma@gmail.com>
2025-05-28 14:40:55 +0200

    update html css and javascript with my prevoius code

--- scripts/tasks/formUtils.js
+++
@@ -1,5 +0,0 @@
-export function resetForm() {
-  document.getElementById("title-input").value = "";
-  document.getElementById("desc-input").value = "";
-  document.getElementById("select-status").value = "todo";
-}
