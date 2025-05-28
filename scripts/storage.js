d79e5b7bb029f5724660fc96f3b31c30fe90fd12
tawandakasunzuma <tskasunzuma@gmail.com>
2025-05-28 17:15:43 +0200

    add JSDoc comments to loadTasks and saveTasks functions

--- scripts/storage.js
+++ scripts/storage.js
@@ -1,10 +1,16 @@
-/** @returns {Array<Task>} */
+/**
+ * Loads tasks from browser localStorage.
+ * @returns {Array<Task>} An array of task objects, or empty array if none are saved in localStorage.
+ */
 export function loadTasks() {
   const jsonString = localStorage.getItem("kanban-tasks");
   return jsonString ? JSON.parse(jsonString) : [];
 }
 
-/** @param {Array<Task>} tasks */
+/**
+ * Saves an array of tasks to the browser's localStorage.
+ * @param {Array<Task>} tasks - The tasks to save.
+ */
 export function saveTasks(tasks) {
   localStorage.setItem("kanban-tasks", JSON.stringify(tasks));
 }
